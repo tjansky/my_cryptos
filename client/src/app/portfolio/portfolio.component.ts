e8f087eefc32d6865f3937fef9ed5b8d53f93bac
tjansky <tomislav.jansky@gmail.com>
2021-03-28 01:05:30 +0100

    fake "api calls" now returns data wraped in observable

--- client/src/app/portfolio/portfolio.component.ts
+++ client/src/app/portfolio/portfolio.component.ts
@@ -16,31 +16,37 @@ export class PortfolioComponent implements OnInit {
   constructor(private portfolioService: PortfolioService, private cryptoDataService: CryptoDataService) { }
 
   ngOnInit(): void {
-    // fot now it is fixed but in future this will be call from db (SUBSCRIBE will be needed)
-    this.addedCoinsIds = this.portfolioService.getAddedCoinsIds();
-    // after we got coinIds we can call api for coin data for each coin
-    this.addedCoinsIds.forEach(cId => {
-      this.cryptoDataService.getCryptoCoinData(cId).subscribe(coin => {
-        // adding coin to array after we got data from api
-        this.addedCoins.push(coin);
-      }, er => {
-        console.log(er);
+    //get IDs from added coins
+    this.portfolioService.getAddedCoinsIds().subscribe(coinIds => {
+      this.addedCoinsIds = coinIds;
+      // after we got coinIds we can call api for coin data for each coin
+      this.addedCoinsIds.forEach(cId => {
+        this.cryptoDataService.getCryptoCoinData(cId).subscribe(coin => {
+          // adding coin to array after we got data from api
+          this.addedCoins.push(coin);
+        }, er => {
+          console.log(er);
+        });
       });
     });
+
+
   }
 
   onCoinAdd() {
     if (this.coinIdToAdd) {
-      // todo: SAVE TO DB COIN ID !!!!
-      this.portfolioService.addCoinIdToDb(this.coinIdToAdd); 
-      
-      // after coinId is saved to dn, get his data nad push it to array
-      this.cryptoDataService.getCryptoCoinData(this.coinIdToAdd).subscribe(coin => {
-        // adding coin to array after we got data from api
-        this.addedCoins.push(coin);
-      }, er => {
-        console.log(er);
+      // after coin id is saved to db
+      this.portfolioService.addCoinIdToDb(this.coinIdToAdd).subscribe(addedCoinId => {  // !!! maybe instead of calling server for new coinid array, we just pass addedCoinId to array !!!
+        // after coinId is saved to dn, get his data nad push it to array
+        this.cryptoDataService.getCryptoCoinData(this.coinIdToAdd).subscribe(coin => {
+          // adding coin to array after we got data from api
+          this.addedCoins.push(coin);
+        }, er => {
+          console.log(er);
+        });
       });
+
+
     }
   }
 
--- client/src/app/portfolio/portfolio.service.ts
+++ client/src/app/portfolio/portfolio.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ICryptoCoin } from '../shared/ICryptoCoin';
-import { Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 
 
 @Injectable({
@@ -9,21 +9,21 @@ import { Subject } from 'rxjs';
 })
 export class PortfolioService {
   // in future this will be retrived from db, for now fixed values
-  private addedCoinsIds: string[] = ["ethereum", "bitcoin", "chainlink"]; 
+  private addedCoinsIds: string[] = ["ethereum", "bitcoin", "chainlink", "dai", "tether", "cardano"]; 
 
   constructor(private http: HttpClient) { }
 
   // in future this will be retrived from db, for now fixed values
-  getAddedCoinsIds(){
-    // return copy of addedCoins array
-    return this.addedCoinsIds.slice();
+  getAddedCoinsIds(): Observable<string[]>{
+    return of(this.addedCoinsIds.slice());
   }
 
-  addCoinIdToDb(coinId: string) {
+  addCoinIdToDb(coinId: string): Observable<string> {
     // add new coinId to array of added coin ids
-    console.log("adding coin id to array: ", coinId);
-    this.addedCoinsIds.push(coinId);
     // in future this will be inserted in db
+    this.addedCoinsIds.push(coinId);
+    return of(coinId);
+    
   }
 
 
