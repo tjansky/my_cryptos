ab370f27bc636a1c59224a04c9ed9ffdb3c78eb5
tjansky <tomislav.jansky@gmail.com>
2021-04-19 18:22:11 +0200

    Added auth midleware to sturtup file (auth working)

--- MyCrypto.Api/Controllers/AddedCoinController.cs
+++ MyCrypto.Api/Controllers/AddedCoinController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyCrypto.Core.IRepositories;
 using MyCrypto.Core.Models;
--- MyCrypto.Api/Startup.cs
+++ MyCrypto.Api/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MyCrypto.Api.Services;
 using MyCrypto.Core.IRepositories;
@@ -55,6 +58,17 @@ namespace MyCrypto.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyCrypto.Api", Version = "v1" });
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +87,7 @@ namespace MyCrypto.Api
 
             app.UseCors();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
